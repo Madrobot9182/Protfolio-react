d55ba45e075e4dbf5caf8b96bd489a662398bb40
Madrobot9182 <65980347+Madrobot9182@users.noreply.github.com>
2024-08-17 13:48:03 -0600

    make templateAreas

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import IntroductionHeader from "./components/IntroductionHeader";
 
 function App() {
   return (
-    <Grid templateAreas={`"header" "main"`} templateRows={"1fr"}>
+    <Grid
+      templateAreas={`"header" "main" "about" "projects" "resume" "contact" "footer"`}
+      templateRows={"10vh 90vh 6fr 6fr 3fr 3fr 1fr"}
+    >
       <GridItem bg="orange.300" area="header">
         <Header />
       </GridItem>
@@ -14,7 +17,36 @@ function App() {
         <Box>
           <IntroductionHeader />
         </Box>
+      </GridItem>
 
+      <GridItem bg="red.300" area="about">
+        <Box>
+          <Text>Placeholder</Text>
+        </Box>
+      </GridItem>
+
+      <GridItem bg="green.300" area="projects">
+        <Box>
+          <Text>Placeholder</Text>
+        </Box>
+      </GridItem>
+
+      <GridItem bg="yellow.300" area="resume">
+        <Box>
+          <Text>Placeholder</Text>
+        </Box>
+      </GridItem>
+
+      <GridItem bg="purple.300" area="contact">
+        <Box>
+          <Text>Placeholder</Text>
+        </Box>
+      </GridItem>
+
+      <GridItem bg="pink.300" area="footer">
+        <Box>
+          <Text>Placeholder</Text>
+        </Box>
       </GridItem>
     </Grid>
   );

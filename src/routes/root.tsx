e9e1fd62664e37d263d62351771ed4bf2fcf2dfb
Madrobot9182 <65980347+Madrobot9182@users.noreply.github.com>
2024-09-23 22:44:24 -0600

    fix about centering

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,12 +1,10 @@
 import {
   Box,
-  Button,
   Grid,
   GridItem,
   Heading,
   Text,
   VStack,
-  Link,
   useMediaQuery,
 } from "@chakra-ui/react";
 import Header from "../components/Header";
@@ -24,10 +22,12 @@ function Root() {
 
   return (
     <Grid
-      templateAreas={{base:`"main" "about" "projects" "resume" "contact" "footer"`, lg:`"main" "about" "projects" "resume" "contact" "footer"`}}
-      templateRows={{base:"100vh", lg:"100vh"}}
+      templateAreas={{
+        base: `"main" "about" "projects" "resume" "contact" "footer"`,
+        lg: `"main" "about" "projects" "resume" "contact" "footer"`,
+      }}
+      templateRows={{ base: "100vh", lg: "100vh" }}
     >
-      
       <GridItem
         area="main"
         id="main"
@@ -36,25 +36,35 @@ function Root() {
         bgPosition="center"
         overflow="hidden"
       >
-        {!isMobile && (
-        <Header />
-      )}
-        
+        {!isMobile && <Header />}
+
         <Box pos="relative" top="30%" left="8%">
           <IntroductionText />
         </Box>
       </GridItem>
 
-      <GridItem bg="gray.700" area="about" id="about" mt="2em" mb="2em">
-        <About />
+      <GridItem
+        bg="gray.700"
+        area="about"
+        id="about"
+        display="flex"
+        justifyContent="center"
+        pt="6em"
+        pb="6em"
+      >
+        <Box
+          maxW={{ base: "100%", lg: "60vw" }}
+        >
+          <About />
+        </Box>
       </GridItem>
 
       <GridItem bg="gray.900" area="projects" id="projects">
         <Box padding={10}>
           <Heading
-            mb="10px"
+            pb="10px"
             as="h1"
-            size={{base: "3xl", lg: "4xl"}}
+            size={{ base: "3xl", lg: "4xl" }}
             noOfLines={1}
             textAlign="center"
           >
@@ -76,18 +86,13 @@ function Root() {
         <Resume />
       </GridItem>
 
-      <GridItem bg="gray.800" area="contact" id="contact" mt="3em">
+      <GridItem bg="gray.800" area="contact" id="contact" pt="3em">
         <VStack>
-          <Heading
-            as="h2"
-            size="3xl"
-            color="aquamarine"
-            noOfLines={1}
-          >
+          <Heading as="h2" size="3xl" color="aquamarine" noOfLines={1}>
             Ryan Yan
           </Heading>
           <Text color="pink.600">if you somehow already forgot</Text>
-          <Heading mt="15px" mb="15px" as="h4" size="md">
+          <Heading pt="15px" pb="15px" as="h4" size="md">
             Check me out below!
           </Heading>
           <ContactGrid />
@@ -95,7 +100,7 @@ function Root() {
       </GridItem>
 
       <GridItem bg="gray.800" area="footer">
-        <Text mt="2em" fontSize={14} align="center">
+        <Text pt="2em" fontSize={14} align="center">
           Copyright by Ryan Yan. Fonts provided by cdnjs.com. Symbols provided
           by fontawesome.com. Various images sourced from pexels.com and
           generated using Stable DIffusion V2
